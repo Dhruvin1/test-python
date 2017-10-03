c25242908447931fdcf6b869052b97f935513048
Dhruvin Shah <dhruvinshah1994@gmail.com>
2017-10-03 11:19:44 -0700

    updated to be used from Jenkins

--- test.py
+++ test.py
@@ -1,19 +1,24 @@
 def committed():
-    print "Committed"
+    print("Committed")
 
 def stagged():
-    print "Stagged"
+    print("Stagged")
 
 def working():
-    print "Working directory"
+    print("Working directory")
 
 def NewAddisionAfterSecondCommit():
-    print "New modifications made after the second commit"
+    print("New modifications made after the second commit")
 
 def MethodAddedFromBranch():
-    print "This is the method added from the branch named child"
+    print("This is the method added from the branch named child")
 
 def AnotherMethodAddedFromBranch():
-    print " this is the method added from the child branch after the first methhod\
-            being successfully pulled to the master"
+    print(" this is the method added from the child branch after the first methhod\
+            being successfully pulled to the master")
 #Adding just a comment
+print("Hello world!")
+print("This repository will be called from Jenkins!!")
+committed()
+stagged()
+working()
